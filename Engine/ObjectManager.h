ca955be45862d2836351a86be91f9fc20e8a42d5
윤석 <kgcaI@kgcaschool.com>
2025-05-08 20:40:06 +0900

    Fix : "Stencil을 사용 여부에 따라 ObjectList를 분리"
    Stencil 기록만 하는 Actor와 Stencil을 사용하는 Actor의 Render 순서를 Pre와 Post로 분리

--- Engine/ObjectManager.h
+++ Engine/ObjectManager.h
@@ -7,6 +7,10 @@ class ObjectManager : public Singleton<ObjectManager>
 	map<UINT, shared_ptr<class AActor>> m_vActorList;
 	vector<shared_ptr<class AActor>> m_vRenderActorList;	// Render ÀÌÈÄ Clear
 
+	// Stencil Check¿ë
+	vector<shared_ptr<class AActor>> m_vPreRenderActorList;		// 
+	vector<shared_ptr<class AActor>> m_vPostRenderActorList;	// 
+
 public:
 	void Tick();
 	void Render();
@@ -15,5 +19,9 @@ public:
 public:
 	void AddActor(shared_ptr<class AActor> _pActor);
 	void AddActorList(vector<shared_ptr<class AActor>> _vActorList);
+
+private:
+	void CheckStencilList();
+	void ClearRenderList();
 };
 
