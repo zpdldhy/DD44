067e7526caddf158033d7446d0095f72fe21385d
세영 오 <kgcaD@kgcaschool.com>
2025-05-11 16:11:23 +0900

    Feat : "초기 커밋"

--- Game/TestSY.cpp
+++ Game/TestSY.cpp
@@ -41,29 +41,29 @@ void TestSY::Init()
 	}
 
 	{
-		m_pActor = make_shared<APawn>();
+		//m_pActor = make_shared<APawn>();
 
-		m_pStaticMesh = UStaticMeshComponent::CreateCube();
-		m_pActor->SetMeshComponent(m_pStaticMesh);
-		m_pActor->SetScale({ 5.0f, 5.0f, 5.0f });
-		m_pActor->SetPosition({ 0.0f, 2.5f, 10.0f });
-		m_pActor->SetRotation({ 0.0f, 0.0f, 0.0f });
+		//m_pStaticMesh = UStaticMeshComponent::CreateCube();
+		//m_pActor->SetMeshComponent(m_pStaticMesh);
+		//m_pActor->SetScale({ 5.0f, 5.0f, 5.0f });
+		//m_pActor->SetPosition({ 0.0f, 2.5f, 10.0f });
+		//m_pActor->SetRotation({ 0.0f, 0.0f, 0.0f });
 
-		shared_ptr<UMaterial> material = make_shared<UMaterial>();
-		material->Load(L"../Resources/Texture/kkongchi.jpg", L"../Resources/Shader/Default.hlsl");
-		m_pStaticMesh->SetMaterial(material);
+		//shared_ptr<UMaterial> material = make_shared<UMaterial>();
+		//material->Load(L"../Resources/Texture/kkongchi.jpg", L"../Resources/Shader/Default.hlsl");
+		//m_pStaticMesh->SetMaterial(material);
 
-		auto pCameraComponent = make_shared<UCameraComponent>();
-		pCameraComponent->SetPosition(Vec3(20.f, 20.f, -20.f));
-		pCameraComponent->SetNear(10.f);
-		pCameraComponent->SetFar(100.f);
-		m_pActor->SetCameraComponent(pCameraComponent);
+		//auto pCameraComponent = make_shared<UCameraComponent>();
+		//pCameraComponent->SetPosition(Vec3(20.f, 20.f, -20.f));
+		//pCameraComponent->SetNear(10.f);
+		//pCameraComponent->SetFar(100.f);
+		//m_pActor->SetCameraComponent(pCameraComponent);
 
-		auto pBoxComponent = make_shared<UBoxComponent>();
-		pBoxComponent->SetScale({ 6.f, 6.f, 6.f });
-		m_pActor->SetShapeComponent(pBoxComponent);
+		//auto pBoxComponent = make_shared<UBoxComponent>();
+		//pBoxComponent->SetScale({ 6.f, 6.f, 6.f });
+		//m_pActor->SetShapeComponent(pBoxComponent);
 
-		m_pActor->AddScript(make_shared<kkongchiMoveScript>());
+		//m_pActor->AddScript(make_shared<kkongchiMoveScript>());
 	}
 
 	{
@@ -269,7 +269,7 @@ void TestSY::Init()
 		OBJECTMANAGER->AddActor(actor);
 	});
 
-	OBJECTMANAGER->AddActor(m_pActor);
+	//OBJECTMANAGER->AddActor(m_pActor);
 	OBJECTMANAGER->AddActor(m_pCameraActor);
 	OBJECTMANAGER->AddActor(m_pSky);	
 }
