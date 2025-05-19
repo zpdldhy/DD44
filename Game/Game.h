d92292cd0bfd7f4d6b626a399b8206beca679dc6
윤석 <kgcaI@kgcaschool.com>
2025-05-19 12:56:34 +0900

    Fix : "game 클라이언트의 코드 되돌림"
    문제가 되지 않는 부분은 다시 되돌림

--- Game/Game.h
+++ Game/Game.h
@@ -4,6 +4,14 @@
 class Game : public IExecute
 {
 public:
+	shared_ptr<class ACameraActor> m_pCameraActor;
+
+	shared_ptr<class UStaticMeshComponent> m_pSkyMesh;
+	shared_ptr<class ASky> m_pSky;
+
+	shared_ptr<class ALight> m_pSunLight;
+
+public:
 	void Init() override;
 	void Update() override;
 	void Render() override;
