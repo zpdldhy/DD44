8bdd68388bb487e2d977355e752e079400f400c3
윤석 <kgcaI@kgcaschool.com>
2025-05-20 22:40:28 +0900

    Feat : "UI Texture 선택 기능 추가"

--- Engine/UIEditorUI.cpp
+++ Engine/UIEditorUI.cpp
@@ -2,6 +2,9 @@
 #include "UIEditorUI.h"
 #include "PrefabLoader.h"
 #include "AUIActor.h"
+#include "UMaterial.h"
+#include "Texture.h"
+#include "Functions.h"
 
 void UIEditorUI::DrawUI()
 {
@@ -15,7 +18,7 @@ void UIEditorUI::DrawUI()
 		// 초기값 세팅
 		m_OnCreate(
 			m_pUIActor,
-			m_szTexturePath,
+			m_vTextureList[0].c_str(),	// white.png
 			m_szShaderPath,
 			m_UIData
 		);
@@ -27,18 +30,23 @@ void UIEditorUI::DrawUI()
 	ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
 	// ───────────────────────────── Transform ─────────────────────────────
 	ImGui::TextColored(ImVec4(1, 1, 0, 1), "Transform");
+
+	ImGui::SameLine(0, 37.5f); ImGui::Text("%s", "X");
+	ImGui::SameLine(0, 72.5f); ImGui::Text("%s", "Y");
+	ImGui::SameLine(0, 72.5f); ImGui::Text("%s", "Z");
+
 	DrawVec3("Position", m_UIData.Position);
 	DrawVec3("Rotation", m_UIData.Rotation);
-	DrawVec3("Scale", m_UIData.Scale);
-
-	// ───────────────────────────── Actor Update ─────────────────────────────
-	UpdateUIActor();
+	DrawVec3("Scale   ", m_UIData.Scale);
 
 	ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
 	// ───────────────────────────── Asset Input ─────────────────────────────
 	ImGui::TextColored(ImVec4(1, 1, 0, 1), "Asset Paths");
-	ImGui::InputText("Texture", m_szTexturePath, IM_ARRAYSIZE(m_szTexturePath));
+	SetTexture();
 	ImGui::InputText("Shader", m_szShaderPath, IM_ARRAYSIZE(m_szShaderPath));
+
+	// ───────────────────────────── Actor Update ─────────────────────────────
+	UpdateUIActor();
 
 	ImGui::Spacing();
 	// ───────────────────────────── Prefab Save/Load ─────────────────────────────
@@ -52,7 +60,7 @@ void UIEditorUI::DrawUI()
 		PrefabObjectData data;
 		data.Name = prefabName;
 		data.ShaderPath = m_szShaderPath;
-		data.TexturePath = m_szTexturePath;
+		//data.TexturePath = m_szTexturePath;
 		data.Scale = Vec3(m_UIData.Scale);
 		data.Rotation = Vec3(m_UIData.Rotation);
 		data.Translation = Vec3(m_UIData.Position);
@@ -67,7 +75,7 @@ void UIEditorUI::DrawUI()
 		if (PREFAB->LoadObject(path, data))
 		{
 			strcpy_s(m_szShaderPath, data.ShaderPath.c_str());
-			strcpy_s(m_szTexturePath, data.TexturePath.c_str());
+			//strcpy_s(m_szTexturePath, data.TexturePath.c_str());
 
 			
 		}
@@ -82,30 +90,19 @@ void UIEditorUI::DrawVec3(const char* label, float* values)
 	ImGui::Text("%s", label);
 	ImGui::Indent(10.0f);
 
-	static const char* axes[] = { "X", "Y", "Z" };
-	const float buttonSize = 20.0f;
 	const float inputWidth = 60.0f;
-	const float sliderWidth = 150.0f;
-	const float spacing = 4.0f;
+	const float spacing = 20.0f;
+
 
 	for (int i = 0; i < 3; ++i)
 	{
-		ImGui::PushID(i);
-
-		ImGui::Text("%s", axes[i]); ImGui::SameLine();
-
-		if (ImGui::Button("-", ImVec2(buttonSize, 0)))
-			values[i] -= 0.1f;
 		ImGui::SameLine(0, spacing);
+
+		ImGui::PushID(i);
 
 		ImGui::PushItemWidth(inputWidth);
 		ImGui::InputFloat("##Value", &values[i], 0.0f, 0.0f, "%.2f");
-		ImGui::PopItemWidth();
-
-		ImGui::SameLine(0, spacing);
-
-		if (ImGui::Button("+", ImVec2(buttonSize, 0)))
-			values[i] += 0.1f;
+		ImGui::PopItemWidth();		
 
 		ImGui::PopID();
 	}
@@ -126,9 +123,72 @@ void UIEditorUI::UpdateUIActor()
 
 void UIEditorUI::ResetData()
 {
+	// Transform
 	for (int i = 0; i < 3; i++) m_UIData.Position[i] = 0.0f;
 	for (int i = 0; i < 3; i++) m_UIData.Rotation[i] = 0.0f;
-	for (int i = 0; i < 3; i++) m_UIData.Scale[i] = 100.0f;
-	strcpy_s(m_szTexturePath, "../Resources/Texture/white.png");
+	for (int i = 0; i < 2; i++) m_UIData.Scale[i] = 100.0f;
+	m_UIData.Scale[2] = 1.0f;
+
+	// Texture
+	m_iSelectedTextureIndex = 0;
+	m_vTextureList.emplace_back("../Resources/Texture/white.png");
+	m_vTextureNameList.emplace_back("white.png");
+
 	strcpy_s(m_szShaderPath, "../Resources/Shader/Default.hlsl");
+}
+
+void UIEditorUI::SetTexture()
+{
+	if (m_vTextureList.size() == 1)
+	{
+		string searchPath = "../Resources/Texture/";
+
+		// 만약 다른 형식의 파일도 필요하다면 여기에 추가
+		SearchFile(searchPath, ".png");
+		SearchFile(searchPath, ".jpg");
+	}
+
+	static std::vector<const char*> NamePtrs;
+	if (NamePtrs.empty())
+	{
+		for (auto& name : m_vTextureNameList)
+			NamePtrs.push_back(name.c_str());
+	}
+
+	static int iPreSelectedIndex = 0;
+
+	ImGui::Combo("Texture", &m_iSelectedTextureIndex, NamePtrs.data(), (int)NamePtrs.size());
+
+	if (m_iSelectedTextureIndex != iPreSelectedIndex)
+	{
+		iPreSelectedIndex = m_iSelectedTextureIndex;
+		m_pUIActor->GetMeshComponent()->GetMaterial()->SetTexture(TEXTURE->Get(to_mw(m_vTextureList[iPreSelectedIndex])));
+	}
+
+}
+
+void UIEditorUI::SearchFile(const string& _directory, const string& _extension)
+{
+	string searchPath = _directory + "*" + _extension;
+
+	WIN32_FIND_DATAA findData;
+	HANDLE hFind = FindFirstFileA(searchPath.c_str(), &findData);
+
+	if (hFind != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+			{
+				// white가 최상단이 되도록
+				if(!strcmp(findData.cFileName, "white.png"))
+					continue;
+
+				m_vTextureList.push_back(_directory + findData.cFileName);
+				m_vTextureNameList.push_back(findData.cFileName);
+			}
+		} while (FindNextFileA(hFind, &findData));
+
+		FindClose(hFind);
+	}
 }
--- Engine/UIEditorUI.h
+++ Engine/UIEditorUI.h
@@ -15,6 +15,10 @@ public:
 private:
 	void UpdateUIActor();
 	void ResetData();
+	void SetTexture();
+
+	vector<string> GetTextureList(const std::string& directory);
+	void SearchFile(const string& directory, const string& extension);
 
 private:
 	shared_ptr<class AUIActor> m_pUIActor = nullptr;
@@ -23,7 +27,9 @@ private:
 								{ 0.0f, 0.0f, 0.0f },
 								{ 100.0f, 100.0f, 1.0f } };
 
-	char m_szTexturePath[256] = "../Resources/Texture/white.png";
+	int m_iSelectedTextureIndex = 0;
+	vector<string> m_vTextureList;
+	vector<string> m_vTextureNameList;
 	char m_szShaderPath[256] = "../Resources/Shader/Default.hlsl";
 
 	std::function<void(shared_ptr<AUIActor>, const char*, const char*, ActorData)> m_OnCreate;
