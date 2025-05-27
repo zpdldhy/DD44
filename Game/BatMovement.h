7c39a8ba176a2492b8ff447371123d6d8f44c975
Nyangrin <joeann12@naver.com>
2025-05-27 15:47:31 +0900

    Chore : "Bat fsm 또 수정"
    공격 후 이동에 대한 처리 더 필요

--- Game/BatMovement.h
+++ Game/BatMovement.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "UScriptComponent.h"
+#include "BatMovement.h"
+#include "BatStates.h"
 
 class BatMovement : public UScriptComponent
 {
@@ -7,14 +9,32 @@ public:
 	BatMovement() = default;
 	virtual ~BatMovement() = default;
 public:
-	Vec3 m_vCenter ;
+	Vec3 m_vCenter;
+	Vec3 m_vTargetPos;
+	Vec3 m_vPrevPos;
+	float m_fLerp = -0.05;
+	float m_fLerpOffset = 0.005f;
 	float m_fRadius = 8.0f;
 	float angle = 0.0f; 
 	float angularSpeed = 1.0f;
 	float rotateSpeed = 0.5f;
 	float m_fRotationSpeed = 8.0f;
 
+	// 
+	bool m_bReturn;
+	Vec3 m_vReturnPos;
+	// States 
+	shared_ptr<StateBase> idle;
+	shared_ptr<StateBase> walk;
+	shared_ptr<StateBase> attack;
+	shared_ptr<StateBase> death;
+	shared_ptr<StateBase> currentState;
+
+
 public:
 	void Init() override;
 	void Tick() override;
+public:
+	void ChangetState(shared_ptr<StateBase> _state);
+	void ReturningToPos();
 };
