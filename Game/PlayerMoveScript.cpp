0f5c46f9c88de1090f26aa133e3e54338e56385f
Nyangrin <joeann12@naver.com>
2025-06-04 14:25:26 +0900

    Chore : "플레이어 로직에 추가"

--- Game/PlayerMoveScript.cpp
+++ Game/PlayerMoveScript.cpp
@@ -216,6 +216,17 @@ void PlayerMoveScript::Tick()
 			// 구르기
 			m_vRollLook = GetOwner()->GetLook();
 			ChangetState(roll);
+
+			// FX
+			Vec3 pos = GetOwner()->GetPosition();
+			Vec3 dir = GetOwner()->GetLook();
+
+			// 살짝 위로 뜨고 살짝 퍼지게 만들기
+			pos.y += 0.3f;
+
+			Vec3 dustVelocity = dir * 20.0f + Vec3(0, 5.0f, 0); // 약간 위로 퍼지게
+			EFFECT->PlayEffect(EEffectType::Feather, pos, 10.0f, dustVelocity);
+
 			m_bCanRoll = false;
 		}
 	}
@@ -257,6 +268,16 @@ void PlayerMoveScript::Tick()
 		{
 			ChangetState(walk);
 		}
+
+		// FX
+		Vec3 pos = GetOwner()->GetPosition();
+		Vec3 dir = GetOwner()->GetLook();
+
+		// 살짝 위로 뜨고 살짝 퍼지게 만들기
+		pos.y += 0.3f;
+
+		Vec3 dustVelocity = dir * 20.0f + Vec3(0, 5.0f, 0); // 약간 위로 퍼지게
+		EFFECT->PlayEffect(EEffectType::Dust, pos, 10.0f, dustVelocity);
 
 		// 이동
 		{
