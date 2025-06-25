2ff43e0735aa30be07144eb6d7a9d472edfb003e
윤석 이 <lyysss1210@naver.com>
2025-06-25 19:07:20 +0900

    Feat : "잠시 UI좀 고치고 올게요"

--- Engine/ACameraActor.cpp
+++ Engine/ACameraActor.cpp
@@ -1,2 +1,9 @@
 #include "pch.h"
 #include "ACameraActor.h"
+
+void ACameraActor::MoveSplineCurve()
+{
+	Vec3 start = Vec3(0.f, 20.f, 0.f);
+	Vec3 middle = Vec3(30.f, 50.f, 0.f);
+	Vec3 end = Vec3(50.f, 20.f, 0.f);
+}
--- Engine/ACameraActor.h
+++ Engine/ACameraActor.h
@@ -7,5 +7,8 @@ class ACameraActor : public AActor
 public:
 	ACameraActor(ProjectionType _type = ProjectionType::PT_PERSPECTIVE) { m_arrComponent[static_cast<size_t>(ComponentType::CT_CAMERA)] = static_pointer_cast<USceneComponent>(make_shared<UCameraComponent>(_type)); }
 	~ACameraActor() {}
+
+public:
+	void MoveSplineCurve();
 };
 
